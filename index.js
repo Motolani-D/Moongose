d0e26e402d22a5077bfad32b611068cfdc888c9e
Motolani-D <motolanid@gmail.com>
2021-12-30 10:11:09 +0100

    query builder

--- index.js
+++ index.js
@@ -121,3 +121,43 @@ app.get("/updateage", (req, res) => {
       console.log(err);
     });
 });
+
+//delete one item by id
+app.get("/removeone", (req, res) => {
+  Profile.findByIdAndRemove({ _id: "61cc6227ab4f637b57df513d" })
+
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+//delete many items by name
+app.get("/removeone", (req, res) => {
+  Profile.remove({ name: "Motolani" })
+
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+//Querybuilder
+app.get("qhelper", (req, res) => {
+  Profile.find({
+    name: "Motolani",
+  })
+    .where("age")
+    .gt(10)
+    .lt(5)
+    .where("favoriteFoods")
+    .in(["rice", "salad"])
+    .limit(2)
+    .sort("-timestamps")
+    .select("name occupation")
+    .exec(callback);
+});
